00651c87fd30c78826668e4f2cc89e6da1bdf004
kyrlian <kyrlian@gmail.com>
2024-10-06 16:01:37 +0200

    draft

--- hills_v2.py
+++ hills_v2.py
@@ -1,24 +1,27 @@
 # DRAFT
+from random import random
 
-def p_change(x:int, y:int):
-   p= .6
-   return p
+def p_change(x: int, y: int) -> float:
+    p = 0.6
+    return p
 
-def p_up_stay_down(x, y):
-   if y < summit:
-      return 0.2, .2, .6
-   else:
-      return .6, .2, .2
 
-def go_up_stay_down(x, y, last):
-     if math.rand(1) > p_change(x,y):
-         return last
-     else:
-         r2=rand(1)
-         d, s, u =  p_up_stay_down(x, y)
-         if r2 < d :
-             return -1
-          elif r2 < s:
-             return 0
-          else :
-             return 1
+def p_up_stay_down(x: int, y: int, summit: int) -> (float, float, float):
+    if y < summit:
+        return 0.2, 0.2, 0.6
+    else:
+        return 0.6, 0.2, 0.2
+
+
+def go_up_stay_down(x: int, y: int, last: int,summit: int) -> int:
+    if random() > p_change(x, y):
+        return last
+    else:
+        r2 = random()
+        d, s, u = p_up_stay_down(x, y,summit)
+        if r2 < d:
+            return -1
+        elif r2 < s:
+            return 0
+        else:
+            return 1
